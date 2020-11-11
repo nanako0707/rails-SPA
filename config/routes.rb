053cc3c879f54693f6bac0cc0c5e4531872726e0
nanako kan <n.k.0213.0529@gmail.com>
2020-11-12 08:21:28 +0900

    add auth to contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :contents
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  resources :contents
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  scope '/api' do
+    get '/contents', to: 'contents#index', defaults: { format: :json }
+    get '/contents/:id', to: 'contents#show', defaults: { format: :json }
+  end
 end
